35222825ebb1d47145926dce0ad80f17c36fafba
Brayan Esteves <besteves@moveapps.cl>
2024-03-09 10:31:13 -0400

    Layout[(index)] - RestaurantCard 'Favorites' (Lazy Loading)[09/03/2024]

--- src/app/components/RestaurantCard.tsx
+++ src/app/components/RestaurantCard.tsx
@@ -1,17 +1,47 @@
-'use client'
+"use client";
 
+import dynamic from "next/dynamic";
 import Link from "next/link";
 
-export default function RestaurantCard({restaurant}: {restaurant: {
-  id: string;
-  name: string;
-  image: string;
-  description: string;
-  score: number;
-  ratings: number;
-}}) {
-  const isFavourite = window.localStorage.getItem('favorites')?.includes(restaurant.id)
+function FavoriteButton({
+  restaurant,
+}: {
+  restaurant: {
+    id: string;
+    name: string;
+    image: string;
+    description: string;
+    score: number;
+    ratings: number;
+  };
+}) {
+  const isFavourite = window.localStorage.getItem("favorites")?.includes(restaurant.id);
 
+  return (
+    <button
+      className={`text-xl text-red-500 ${isFavourite ? "opacity-100" : "opacity-20"}`}
+      type="button"
+    >
+      ♥
+    </button>
+  );
+}
+
+// Creamos un componente dinámico para que no se renderice en el servidor
+const DynamicFavoriteButton = dynamic(async () => FavoriteButton, {ssr: false});
+
+export default function RestaurantCard({
+  restaurant,
+}: {
+  restaurant: {
+    id: string;
+    name: string;
+    image: string;
+    description: string;
+    score: number;
+    ratings: number;
+  };
+}) {
   return (
     <article>
       <img
@@ -19,7 +49,7 @@ export default function RestaurantCard({restaurant}: {restaurant: {
         className="mb-3 h-[300px] w-full object-cover"
         src={restaurant.image}
       />
-      <h2 className="inline-flex gap-2 text-lg font-bold items-center">
+      <h2 className="inline-flex items-center gap-2 text-lg font-bold">
         <Link href={`/${restaurant.id}`}>
           <span>{restaurant.name}</span>
         </Link>
@@ -28,7 +58,7 @@ export default function RestaurantCard({restaurant}: {restaurant: {
           <span>{restaurant.score}</span>
           <span className="font-normal opacity-75">({restaurant.ratings})</span>
         </small>
-        <button type="button" className={`text-red-500 text-xl ${isFavourite ? 'opacity-100' : 'opacity-20'}`}>♥</button>
+        <DynamicFavoriteButton restaurant={restaurant} />
       </h2>
       <p className="opacity-90">{restaurant.description}</p>
     </article>
