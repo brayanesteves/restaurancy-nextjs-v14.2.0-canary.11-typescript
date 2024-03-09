ffb582a893407ae705ac9e3a8ec39287de0f4635
Brayan Esteves <besteves@moveapps.cl>
2024-03-09 10:22:41 -0400

    Layout[(index)] - Search Actions [09/03/2024]

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import {redirect} from "next/navigation";
 
 import api from "@/api";
 
@@ -14,9 +15,21 @@ export default async function HomePage({searchParams}: {searchParams: {q: string
 
   const restaurants = await api.search(searchParams.q);
 
+  async function searchAction(formData: FormData) {
+    "use server";
+
+    redirect(`/?q=${formData.get("query")}`);
+  }
+
   return (
     <section>
-      <SearchBox />
+      {/*<SearchBox />*/}
+      <form action={searchAction} className="mb-4 inline-flex gap-2">
+        <input className="px-2" defaultValue={searchParams.q || ""} name="query" />
+        <button className="bg-white/20 p-2" type="submit">
+          Search
+        </button>
+      </form>
       <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3">
         {restaurants.map((restaurant) => {
           return (
