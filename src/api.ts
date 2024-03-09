2b4f599299596cb2c5756fa6c001dac41d2fa4fd
Brayan Esteves <besteves@moveapps.cl>
2024-03-09 10:01:19 -0400

    Layout[app] - Revalidate cache[09/03/2024]

--- src/api.ts
+++ src/api.ts
@@ -133,12 +133,39 @@ const api = {
     return restaurants;
   },
 
-  list: async (): Promise<Restaurant[]> => {
+  listClearCacheAutomatic: async (): Promise<Restaurant[]> => {
     // Obtenemos la información de Google Sheets en formato texto y la dividimos por líneas, nos saltamos la primera línea porque es el encabezado
     const [, ...data] = await fetch(
       `https://docs.google.com/spreadsheets/d/e/2PACX-1vRT6y8q8xKbymv9HzCRqeEO6n8EeXaiiAJekMx1aXQiIbQNmqkZlDR_Sn3gangCHb_9NHfnOi8Dr421/pub?output=csv`,
       // {cache: "no-store"},
       {next: {revalidate: 100}},
+    )
+      .then((res) => res.text())
+      .then((text) => text.split("\n"));
+
+    // Convertimos cada línea en un objeto Restaurant, asegúrate de que los campos no posean `,`
+    const restaurants: Restaurant[] = data.map((row) => {
+      const [id, name, description, address, score, ratings, image] = row.split(",");
+
+      return {
+        id,
+        name,
+        description,
+        address,
+        score: Number(score),
+        ratings: Number(ratings),
+        image,
+      };
+    });
+
+    // Lo retornamos
+    return restaurants;
+  },
+
+  list: async (): Promise<Restaurant[]> => {
+    // Obtenemos la información de Google Sheets en formato texto y la dividimos por líneas, nos saltamos la primera línea porque es el encabezado
+    const [, ...data] = await fetch(
+      `https://docs.google.com/spreadsheets/d/e/2PACX-1vRT6y8q8xKbymv9HzCRqeEO6n8EeXaiiAJekMx1aXQiIbQNmqkZlDR_Sn3gangCHb_9NHfnOi8Dr421/pub?output=csv`,
     )
       .then((res) => res.text())
       .then((text) => text.split("\n"));
